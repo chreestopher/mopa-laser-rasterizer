69ca8aa5fc8b7a456d573f07a1ecbf8cdde3c7e1
Chris <chreestopher@protonmail.com>
2026-08-13 08:54:30 -0400

    init holographic etching lab experiment page

--- routes/home.py
+++ routes/home.py
@@ -1,6 +1,6 @@
 """Static-page routes."""
 
-from flask import current_app
+from flask import current_app, render_template
 
 from . import routes
 
@@ -8,3 +8,9 @@ from . import routes
 @routes.route("/")
 def index():
     return current_app.send_static_file("index.html")
+
+
+@routes.route("/holographic-etching")
+def holographic_etching():
+    """Dedicated workspace for the structural-color engraving workflow."""
+    return render_template("holographic_etching.html")
